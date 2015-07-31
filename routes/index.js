46ee222f77e61d95b3bfeaf0cd21c7707046e1b7
Evilcome <evilcome.com@gmail.com>
2015-07-31 12:50:48 +0800

    add custom 404 page

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,10 @@ var importRoutes = keystone.importer(__dirname);
 keystone.pre('routes', middleware.initLocals);
 keystone.pre('render', middleware.flashMessages);
 
+keystone.set('404', function(req, res, next) {
+	res.status(404).render('404');
+});
+
 // Import Route Controllers
 var routes = {
 	views: importRoutes('./views'),
